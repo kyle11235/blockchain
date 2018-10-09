6ae15a101b8965e34779325767d0e7a0eb24f042
kyle11235 <aizuguo_@126.com>
2018-10-09 13:45:16 +0800

    http basic for node proxy

--- proxy/proxy.js
+++ proxy/proxy.js
@@ -1,27 +1,37 @@
 var express = require('express');
 var request = require('request');
+var Buffer = require('buffer/').Buffer;
+
 var app = express();
 
 app.use('/', function (req, res) {
-	var url = req.query.url;
-	console.log('real url=' + req.query.url);
-	
-	res.setHeader('Access-Control-Allow-Origin', '*');
-	res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
-	res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
-	res.setHeader('Access-Control-Allow-Credentials', true);
-	res.setHeader('Content-Type', 'application/json;charset=UTF-8');
+    let url = req.query.url;
 
-	if (req.method === 'OPTIONS') {
-		console.log('\nOPTIONS method.');
-		res.send('');
-	} else {
-		var resProxy = res;
-		var options = {
-			url: url
-		};
-		req.pipe(request(options)).pipe(resProxy);
-	}
+    const username = '';
+    const password = '';
+    let auth = "Basic " + Buffer.from(username + ":" + password).toString("base64");
+
+    console.log('real url=' + req.query.url);
+
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+    res.setHeader('Access-Control-Allow-Credentials', true);
+    res.setHeader('Content-Type', 'application/json;charset=UTF-8');
+
+    if (req.method === 'OPTIONS') {
+        console.log('\nOPTIONS method.');
+        res.send('');
+    } else {
+        var resProxy = res;
+        var options = {
+            url: url,
+            headers : {
+                "Authorization" : auth
+            }
+        };
+        req.pipe(request(options)).pipe(resProxy);
+    }
 
 });
 console.log('running at 8080...');
